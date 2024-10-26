e73b33a3d6d7ecccc2a98407876ea3b03be5fe97
kieran crowley <kierancrowley@hotmail.co.uk>
2024-10-26 03:26:59 +0100

    small changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import "./App.css";
-// import TrackList from "./compononets/Tracklist/Tracklist";
+import React from "react";
 import PlayList from "./compononets/Playlist/Playlist";
+import TrackList from "./compononets/Tracklist/Tracklist";
 
 function App() {
-  const trackList = [
+  const tracks = [
     { id: 1, song: "kieran-can-sing", artist: "kieran", album: "kieransAlbum" },
     { id: 2, song: "sarin-can-sing", artist: "sarin", album: "sarinsAlbum" },
-    {
-      id: 3,
-      song: "annabeth-can-sing",
-      artist: "annabeth",
-      album: "annabethsAlbum",
-    },
+    { id: 3, song: "annabeth-can-sing", artist: "annabeth", album: "annabethsAlbum" },
   ];
 
-  const playList = `kierans family charts`;
+  const userAddsName = `kierans family charts`;
+  const userAddsSong = [tracks[0], tracks[2]]
 
   return (
     <>
-      {/* <TrackList data={trackList}></TrackList> */}
-      <PlayList playlistname={playList} tracks={trackList}></PlayList>
+      <TrackList data={tracks}></TrackList>
+      <PlayList playlistname={userAddsName} tracks={userAddsSong}></PlayList>
     </>
   );
 }
 export default App;
+
+// Your Jammming web app should allow users to add songs from the search results to their custom playlist.
+// implement a method that adds a selected song from the search results track list to the user’s custom playlist.
+// The method should be triggered when the user clicks an “add” button displayed next to each track in the search results list.
